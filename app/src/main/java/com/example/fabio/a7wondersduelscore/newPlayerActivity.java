ca2e5991898315d9df83a8efc6656937c982f0d8
fabio92rz <fabio92rz@gmail.com>
2016-11-30 18:25:54 +0100

    Updating front end

---
+++ app/src/main/java/com/example/fabio/a7wondersduelscore/newPlayerActivity.java
@@ -0,0 +1,32 @@
+package com.example.fabio.a7wondersduelscore;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by fabio on 30/11/2016.
+ */
+
+public class newPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newplayer_popup);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width * 0.8), (int) (height * 0.4));
+
+    }
+
+
+}
+
+
+
+
--- app/src/main/java/com/example/fabio/a7wondersduelscore/playersActivity.java
+++ app/src/main/java/com/example/fabio/a7wondersduelscore/playersActivity.java
@@ -1,23 +1,45 @@
 package com.example.fabio.a7wondersduelscore;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * Created by fabio on 29/11/2016.
  */
 
-public class playersActivity extends Fragment {
+public class playersActivity extends Fragment implements View.OnClickListener {
+
+    FloatingActionButton addPlayer;
 
     //Overriden method onCreateView
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        //Returning the layout file after inflating
-        //Change R.layout.tab1 in you classes
-        return inflater.inflate(R.layout.players_tab, container, false);
+        View v = inflater.inflate(R.layout.players_tab, container, false);
+
+        addPlayer = (FloatingActionButton) v.findViewById(R.id.new_player);
+        addPlayer.setOnClickListener(this);
+
+        return v;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == addPlayer){
+
+            Intent newPlayer = new Intent(v.getContext(), newPlayerActivity.class);
+            v.getContext().startActivity(newPlayer);
+
+        }
+
+
     }
 }
